648f3e66c49c0fce71e6c2e3ce0bfd07081d64fa
Sergii <dykyy.s@gmail.com>
2024-08-15 21:00:19 +0300

    add 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-// import Props from './pages/Props/Props';
-// import Points from './pages/Points/Points';
+import Props from './pages/Props/Props';
+import Points from './pages/Points/Points';
 import Quize from './pages/Quize/Quize';
-// import Todos from './pages/Todos/Todos';
+import Todos from './pages/Todos/Todos';
 // import Photos from './pages/Photos/Photos';
 import HomePage from './pages/HomePage/HomePage';
 import { Header } from './components';
@@ -18,9 +18,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         {/* <Route path="/photos" element={<Photos />} /> */}
         <Route path="/quize" element={<Quize />} />
-        {/* <Route path='/points' element={<Points />} /> */}
-        {/* <Route path='/props' element={<Props />} /> */}
-        {/* <Route path='/todos' element={<Todos />} /> */}
+        <Route path="/points" element={<Points />} />
+        <Route path="/props" element={<Props />} />
+        <Route path="/todos" element={<Todos />} />
         {/* <Route path='/countries' element={<Countries />} /> */}
         {/* <Route path='/searchCountries' element={<SearchCountries />} /> */}
         {/* <Route path='*' element={<Navigate to='/' />} /> */}
