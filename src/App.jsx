5214fe31f76c64a3b8b94d4c19646a044a5feb72
Sergii <dykyy.s@gmail.com>
2024-08-15 20:52:06 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@
 // import Todos from './pages/Todos/Todos';
 // import Photos from './pages/Photos/Photos';
 // import HomePage from './pages/HomePage/HomePage';
-// import { Header } from './components';
+import { Header } from './components';
 // import Countries from './pages/Countries/Countries';
 // import SearchCountries from './pages/SearchCountries/SearchCounties';
 
 function App() {
   return (
     <>
-      <h1>app with BrowserRouter </h1>
-      {/* <Header />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/photos' element={<Photos />} />
-				<Route path='/quize' element={<Quize />} />
-				<Route path='/points' element={<Points />} />
-				<Route path='/props' element={<Props />} />
-				<Route path='/todos' element={<Todos />} />
-				<Route path='/countries' element={<Countries />} />
-				<Route path='/searchCountries' element={<SearchCountries />} />
-				<Route path='*' element={<Navigate to='/' />} />
-			</Routes> */}
+      <h1>app with BrowserRouter and Header</h1>
+      <Header />
+      {/* <Routes> */}
+      {/* <Route path='/' element={<HomePage />} /> */}
+      {/* <Route path='/photos' element={<Photos />} /> */}
+      {/* <Route path='/quize' element={<Quize />} /> */}
+      {/* <Route path='/points' element={<Points />} /> */}
+      {/* <Route path='/props' element={<Props />} /> */}
+      {/* <Route path='/todos' element={<Todos />} /> */}
+      {/* <Route path='/countries' element={<Countries />} /> */}
+      {/* <Route path='/searchCountries' element={<SearchCountries />} /> */}
+      {/* <Route path='*' element={<Navigate to='/' />} /> */}
+      {/* </Routes> */}
     </>
   );
 }
