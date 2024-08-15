693228471ababcde5a92cc50537eaf457051d9f6
Sergii <dykyy.s@gmail.com>
2024-08-15 21:17:37 +0300

    commonjsOptions

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,22 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		sourcemap: true,
-	},
-	resolve: {
-		alias: {
-			components: '/src/components',
-			data: '/src/data',
-			styles: '/src/styles',
-			tabs: '/src/tabs',
-			hooks: '/src/hooks',
-			helpers: '/src/helpers',
-			services: '/src/services',
-		},
-	},
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    alias: {
+      components: '/src/components',
+      data: '/src/data',
+      styles: '/src/styles',
+      tabs: '/src/tabs',
+      hooks: '/src/hooks',
+      helpers: '/src/helpers',
+      services: '/src/services',
+    },
+  },
 });
