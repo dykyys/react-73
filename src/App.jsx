8002fb150f63595d235f1a73c0d7d5345878f74e
Sergii <dykyy.s@gmail.com>
2024-08-15 20:57:10 +0300

    del photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 // import Props from './pages/Props/Props';
 // import Points from './pages/Points/Points';
-// import Quize from './pages/Quize/Quize';
+import Quize from './pages/Quize/Quize';
 // import Todos from './pages/Todos/Todos';
 import Photos from './pages/Photos/Photos';
 import HomePage from './pages/HomePage/HomePage';
@@ -16,8 +16,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/photos" element={<Photos />} />
-        {/* <Route path='/quize' element={<Quize />} /> */}
+        {/* <Route path="/photos" element={<Photos />} /> */}
+        {/* <Route path="/quize" element={<Quize />} /> */}
         {/* <Route path='/points' element={<Points />} /> */}
         {/* <Route path='/props' element={<Props />} /> */}
         {/* <Route path='/todos' element={<Todos />} /> */}
